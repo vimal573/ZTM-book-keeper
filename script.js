be300fcf1f905a821b0846906fe055f54aacc973
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-06-16 11:00:26 +0530

    Handling form data and validiate with Regx

--- script.js
+++ script.js
@@ -18,6 +18,41 @@ modalShow.addEventListener('click', showModal);
 modalClose.addEventListener('click', () =>
   modal.classList.remove('show-modal')
 );
-window.addEventListener('click', e =>
-  e.target === modal ? modal.classList.remove('show-modal') : false
-);
+window.addEventListener('click', e => {
+  if (e.target === modal) {
+    modal.classList.remove('show-modal');
+  }
+});
+
+// Validate Form
+function validate(nameValue, urlValue) {
+  const expression =
+    /(https)?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/g;
+  const regex = new RegExp(expression);
+  if (!nameValue || !urlValue) {
+    alert('Please submit values for both fields.');
+    return false;
+  }
+  if (!urlValue.match(regex)) {
+    alert('Please provide a valid web address.');
+    return false;
+  }
+  // Valid
+  return true;
+}
+
+// Handle Data form Form
+function storeBookmark(e) {
+  e.preventDefault();
+  const nameValue = websiteNameEl.value;
+  let urlValue = websiteUrlEl.value;
+  if (!urlValue.includes('http://', 'https://')) {
+    urlValue = `https://${urlValue}`;
+  }
+  if (!validate(nameValue, urlValue)) {
+    return false;
+  }
+}
+
+// Event Listener
+bookmarkForm.addEventListener('submit', storeBookmark);
